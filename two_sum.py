ac442e47d4648a967a2e7e8f05dbd60d3945cfd4
YJMYP <2994762833@qq.com>
2026-08-14 22:45:35 +0800

    Day3 practice: two sum (hash map)

---
+++ two_sum.py
@@ -0,0 +1,11 @@
+﻿def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))
+print(two_sum([3, 2, 4], 6))
